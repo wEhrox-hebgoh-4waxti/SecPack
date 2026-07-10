1720cbd3e01385faefca67f4504973a30e65618d
wEhrox-hebgoh-4waxti <Aliakbarrazabadi333@gmail.com>
2026-07-10 04:33:38 +0330

    Update language.js

--- language.js
+++ language.js
@@ -1,1 +1,78 @@
 
+let currentLanguage = "en";
+
+
+async function loadLanguage(lang){
+
+
+currentLanguage = lang;
+
+
+let file;
+
+
+if(lang==="fa"){
+file="fa.json";
+}
+
+else if(lang==="ar"){
+file="ar.json";
+}
+
+else{
+file="en.json";
+}
+
+
+
+const response =
+await fetch(file);
+
+
+
+const translations =
+await response.json();
+
+
+
+document
+.querySelectorAll("[data-lang]")
+.forEach(element=>{
+
+
+const key =
+element.getAttribute("data-lang");
+
+
+if(translations[key]){
+
+
+element.innerText =
+translations[key];
+
+
+}
+
+
+});
+
+
+
+if(lang==="fa" || lang==="ar"){
+
+document.body.style.direction="rtl";
+
+document.body.style.textAlign="right";
+
+}
+
+else{
+
+document.body.style.direction="ltr";
+
+document.body.style.textAlign="left";
+
+}
+
+
+}
