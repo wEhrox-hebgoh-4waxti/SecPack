d97b6bdebda5ffe0f737ac884e91de32a5a9f2bd
wEhrox-hebgoh-4waxti <Aliakbarrazabadi333@gmail.com>
2026-07-15 16:06:03 +0330

    Update language.js

--- language.js
+++ language.js
@@ -1,1 +1,76 @@
+let translations = {};
 
+
+async function loadLanguage(lang){
+
+try{
+
+const response = await fetch(
+`lang/${lang}.json`
+);
+
+
+translations = await response.json();
+
+
+document
+.querySelectorAll("[data-i18n]")
+.forEach(item=>{
+
+
+let key=item.getAttribute("data-i18n");
+
+
+if(translations[key]){
+
+item.innerText =
+translations[key];
+
+}
+
+
+});
+
+
+if(lang==="fa"){
+
+document.documentElement.dir="rtl";
+
+}
+
+else{
+
+document.documentElement.dir="ltr";
+
+}
+
+
+}
+
+catch(error){
+
+console.log(error);
+
+}
+
+
+}
+
+
+
+function changeLanguage(lang){
+
+loadLanguage(lang);
+
+}
+
+
+
+document.addEventListener(
+"DOMContentLoaded",
+()=>{
+
+loadLanguage("en");
+
+}
+);
